35157b4cf82c62c828d448bdfdc16a0b0e7d5369
AndreaCollura <andrea.collur@gmail.com>
2023-04-11 14:17:29 +0200

    mail list printed

---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            randomMail: []
+
+        }
+    },
+    methods: {
+        getData() {
+                for(let i = 0; i < 10 ; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((mail)=>{
+                    console.log(mail.data.response);
+                    
+                    this.randomMail.push(mail.data.response);
+                    });
+                this.randomMail = [];
+                
+                
+            }
+            
+            
+        }
+
+    },
+    mounted() {
+        // this.getData();
+
+    },
+}).mount('#app');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
